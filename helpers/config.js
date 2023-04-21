eae9a117ece4530768465ead61fb3dbbc2499201
Sonia Neimark <name.ark@gmail.com>
2023-04-21 15:59:51 +0300

    connect to new db

--- helpers/config.js
+++ helpers/config.js
@@ -6,8 +6,8 @@ const {
   PORT,
   JWT_SECRET,
 } = process.env;
-const dbUri = NODE_ENV === 'production' ? DB_URI : 'mongodb://localhost:27017/devdb';
-const secret = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
+const dbUri = NODE_ENV === 'production' ? 'https://eu-central-1.aws.data.mongodb-api.com/app/data-ppkzz/endpoint/data/v1' : 'mongodb://localhost:27017/devdb';
+const secret = NODE_ENV === 'production' ? 'dev-secret' : 'dev-secret';
 const port = NODE_ENV === 'production' ? PORT : 3000;
 
 module.exports = {
